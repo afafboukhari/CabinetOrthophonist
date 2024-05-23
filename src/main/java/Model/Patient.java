15dd7f6c5cb686ef388e8447e9cea7539d1d9bec
mimene <amerimene3948@gmail.com>
2024-05-23 07:56:13 +0100

    patient class

--- src/main/java/Model/Patient.java
+++ src/main/java/Model/Patient.java
@@ -1,4 +1,73 @@
 package Model;
+import java.time.LocalDate;
 
 public class Patient {
+
+    private String nom;
+    private String prenom;
+    private LocalDate dateNaissance;
+    private String lieuNaissance;
+    private String adresse;
+
+    // Constructeur avec tous les paramètres
+    public Patient(String nom, String prenom, LocalDate dateNaissance, String lieuNaissance, String adresse) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.dateNaissance = dateNaissance;
+        this.lieuNaissance = lieuNaissance;
+        this.adresse = adresse;
+    }
+
+    // Getters et setters pour chaque attribut
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public LocalDate getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(LocalDate dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public String getLieuNaissance() {
+        return lieuNaissance;
+    }
+
+    public void setLieuNaissance(String lieuNaissance) {
+        this.lieuNaissance = lieuNaissance;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    // Méthode toString pour afficher les informations du patient
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", dateNaissance=" + dateNaissance +
+                ", lieuNaissance='" + lieuNaissance + '\'' +
+                ", adresse='" + adresse + '\'' +
+                '}';
+    }
 }
